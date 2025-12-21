0c02274e5146c4eeef72c188d69c9f0a3e949ee8
bala-amypo <bala@amypo.in>
2025-12-21 13:26:01 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
@@ -1,11 +1,48 @@
-package com.example.demo;
+package com.example.demo.service.impl;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
+import org.springframework.stereotype.Service;
 
-@SpringBootApplication
-public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public StudentEntity postData(StudentEntity stu) {
+        return studentRepository.save(stu);
+    }
+
+    @Override
+    public List<StudentEntity> getAllData() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public StudentEntity getData(int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public StudentEntity updateData(int id, StudentEntity entity) {
+        if (studentRepository.existsById(id)) {
+            entity.setId(id);
+            return studentRepository.save(entity);
+        }
+        return null;
+    }
+
+    @Override
+    public String deleteData(int id) {
+        studentRepository.deleteById(id);
+        return "Deleted Successfully";
     }
 }
