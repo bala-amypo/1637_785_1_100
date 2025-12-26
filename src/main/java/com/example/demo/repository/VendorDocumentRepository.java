1a07fc4ed1d572e16ab11028899b96ba8c3e3c1a
bala-amypo <bala@amypo.in>
2025-12-26 12:52:34 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorDocumentRepository.java
+++ src/main/java/com/example/demo/repository/VendorDocumentRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.model.VendorDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.query.ModifyingQuery;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +13,11 @@ public interface VendorDocumentRepository extends JpaRepository<VendorDocument, 
 
     @Modifying
     @Transactional
-    @ModifyingQuery
-    void setVendorId(@Param("vendorId") Long vendorId);
+    @Query("UPDATE VendorDocument v SET v.vendor.id = :vendorId WHERE v.id = :id")
+    void setVendorId(@Param("vendorId") Long vendorId, @Param("id") Long id);
 
     @Modifying
     @Transactional
-    @ModifyingQuery
-    void setDocumentTypeId(@Param("documentTypeId") Long documentTypeId);
+    @Query("UPDATE VendorDocument v SET v.documentTypeId = :documentTypeId WHERE v.id = :id")
+    void setDocumentTypeId(@Param("documentTypeId") Long documentTypeId, @Param("id") Long id);
 }
