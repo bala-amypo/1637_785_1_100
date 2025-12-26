18980bd13e161f80bc152a7d554d99b17b8c9897
bala-amypo <bala@amypo.in>
2025-12-26 12:43:34 +0000

    User commit

--- src/main/java/com/example/demo/model/VendorDocument.java
+++ src/main/java/com/example/demo/model/VendorDocument.java
@@ -1,3 +1,11 @@
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 @Entity
 public class VendorDocument {
 
