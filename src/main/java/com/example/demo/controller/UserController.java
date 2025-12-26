f082b67c523a1d5e5a669303a9d65cbc1f7333e6
bala-amypo <bala@amypo.in>
2025-12-26 10:56:12 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,21 +1,19 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.User;
+import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private final UserService service;
-
-    public UserController(UserService service) {
-        this.service = service;
-    }
+    @Autowired
+    private UserService userService;
 
     @PostMapping
-    public User create(@RequestBody User user) {
-        return service.save(user);
+    public User saveUser(@RequestBody User user) {
+        return userService.save(user);
     }
 }
--- src/main/java/com/example/demo/service/Impl/DocumentTypeServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/DocumentTypeServiceImpl.java
@@ -1,11 +1,7 @@
-package com.example.demo.service.impl;
+package com.example.demo.service.Impl;
 
-import com.example.demo.repository.DocumentTypeRepository;
 import com.example.demo.service.DocumentTypeService;
 
 public class DocumentTypeServiceImpl implements DocumentTypeService {
-
-    public DocumentTypeServiceImpl(DocumentTypeRepository repository) {
-    }
+    
 }
-A
