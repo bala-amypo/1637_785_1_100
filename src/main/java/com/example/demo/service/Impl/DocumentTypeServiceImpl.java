0fa9abcaff7e22eec15e5261076391b4d6648d91
bala-amypo <bala@amypo.in>
2025-12-26 10:50:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/DocumentTypeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.repository.DocumentTypeRepository;
+import com.example.demo.service.DocumentTypeService;
+
+public class DocumentTypeServiceImpl implements DocumentTypeService {
+
+    public DocumentTypeServiceImpl(DocumentTypeRepository repository) {
+    }
+}
