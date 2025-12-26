0d7018cc45aa95a7dc0884d38dfe0918c2c228a9
bala-amypo <bala@amypo.in>
2025-12-26 13:01:34 +0000

    User commit

--- src/main/java/com/example/demo/model/ComplianceScore.java
+++ src/main/java/com/example/demo/model/ComplianceScore.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 public class ComplianceScore {
@@ -14,6 +16,10 @@ public class ComplianceScore {
 
     private double score;
 
+    @ManyToOne
+    @JoinColumn(name = "vendor_id")
+    private Vendor vendor;
+
     public ComplianceScore() {}
 
     public ComplianceScore(Long id, double score) {
@@ -21,6 +27,7 @@ public class ComplianceScore {
         this.score = score;
     }
 
+    // Existing getters/setters...
     public Long getId() {
         return id;
     }
@@ -35,5 +42,14 @@ public class ComplianceScore {
 
     public void setScore(double score) {
         this.score = score;
+    }
+
+    // ADD THESE for vendor
+    public Vendor getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(Vendor vendor) {
+        this.vendor = vendor;
     }
 }
