fe2cd951f1273ff4ea7798663efe55aa389b0766
bala-amypo <bala@amypo.in>
2025-12-26 10:17:12 +0000

    User commit

--- src/main/java/com/example/demo/repository/ComplianceRule.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.ComplianceRule;
-
-public interface ComplianceRuleRepository extends JpaRepository<ComplianceRule, Long> {
-
-    boolean existsByRuleName(String ruleName);
-}
