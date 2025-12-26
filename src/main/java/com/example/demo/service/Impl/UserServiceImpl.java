c82900c21c5a52577475e04217a959123179e734
bala-amypo <bala@amypo.in>
2025-12-26 09:50:12 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -1,26 +1,42 @@
 package com.example.demo.service.impl;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.example.demo.entity.User;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository repository;
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository repository,
+                           PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
-    public User saveUser(User user) {
+    public User registerUser(User user) {
+        if (repository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException("Email already used");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
 
     @Override
-    public List<User> getAllUsers() {
-        return repository.findAll();
+    public User findByEmail(String email) {
+        return repository.findByEmail(email)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User not found"));
+    }
+
+    @Override
+    public User getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User not found"));
     }
 }
