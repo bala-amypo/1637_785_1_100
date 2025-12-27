b10c38adff2260b55d9004d661f472fa38a6ab84
bala-amypo <bala@amypo.in>
2025-12-27 08:29:29 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ComplianceRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ComplianceRuleServiceImpl.java
@@ -1,17 +1,19 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.model.ComplianceRule;
+import com.example.demo.repository.ComplianceRuleRepository;
 import com.example.demo.service.ComplianceRuleService;
-import org.springframework.stereotype.Service;
 
-@Service
 public class ComplianceRuleServiceImpl implements ComplianceRuleService {
+
+    private final ComplianceRuleRepository repository;
+
+    public ComplianceRuleServiceImpl(ComplianceRuleRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public ComplianceRule save(ComplianceRule rule) {
-       
-        return rule; 
+        return repository.save(rule);
     }
-
-   
 }
