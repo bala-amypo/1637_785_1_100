c3d133b5f8b38d6e893f224c8111070cf4e7f55d
bala-amypo <bala@amypo.in>
2025-12-27 09:09:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorDocumentRepository.java
+++ src/main/java/com/example/demo/repository/VendorDocumentRepository.java
@@ -1,4 +1,6 @@
 package com.example.demo.repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
