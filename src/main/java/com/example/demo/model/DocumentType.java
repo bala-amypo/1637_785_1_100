c95fb273e5aa75f43f1af33315218408a96227b9
bala-amypo <bala@amypo.in>
2025-12-28 05:38:29 +0000

    User commit

--- src/main/java/com/example/demo/model/DocumentType.java
+++ src/main/java/com/example/demo/model/DocumentType.java
@@ -2,29 +2,37 @@ package com.example.demo.model;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "document_types")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)  // Only include fields marked with @EqualsAndHashCode.Include
 public class DocumentType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include  // Explicitly include this field
     private Long id;
 
+    @EqualsAndHashCode.Include  // Explicitly include this field
     private int weight;
+    
+    @EqualsAndHashCode.Include  // Explicitly include this field
     private Boolean required;
 
+    @EqualsAndHashCode.Include  // Explicitly include this field
     private LocalDateTime createdAt;
 
     @ManyToMany(mappedBy = "supportedDocumentTypes")
-    private List<Vendor> vendors = new ArrayList<>();  // Changed from Set to List
+    private List<Vendor> vendors = new ArrayList<>();  // This field is NOT included in equals/hashCode
 
     @PrePersist
     public void prePersist() {
@@ -34,22 +42,5 @@ public class DocumentType {
     public void addVendor(Vendor vendor) {
         this.vendors.add(vendor);
         vendor.getSupportedDocumentTypes().add(this);
-    }
-    
-    // Override equals and hashCode to avoid infinite recursion
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DocumentType that = (DocumentType) o;
-        return weight == that.weight &&
-               Objects.equals(id, that.id) &&
-               Objects.equals(required, that.required) &&
-               Objects.equals(createdAt, that.createdAt);
-    }
-    
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, weight, required, createdAt);
     }
 }
