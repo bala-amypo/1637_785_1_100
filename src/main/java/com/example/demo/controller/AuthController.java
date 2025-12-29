5f7a7e86234fc8296af87c70f0f8bb842c6a9ae8
bala-amypo <bala@amypo.in>
2025-12-29 07:58:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.model.User;
+import com.example.demo.service.impl.UserServiceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody User user) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getEmail(),
+                        user.getPassword()
+                )
+        );
+
+        User dbUser = userService.findByEmail(user.getEmail());
+
+        String token = "token";
+
+        return new AuthResponse(
+                token,
+                dbUser.getId(),
+                dbUser.getEmail(),
+                dbUser.getRole()
+        );
+    }
+}
