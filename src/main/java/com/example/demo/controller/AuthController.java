9a7d322f7257f0f1041b45454a4db1544d56de66
bala-amypo <bala@amypo.in>
2025-12-29 08:35:43 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -5,44 +5,27 @@ import com.example.demo.model.User;
 import com.example.demo.service.impl.UserServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 
 @RestController
+@SecurityRequirements
 @RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
     private UserServiceImpl userService;
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    // -------------------------
-    // REGISTER
-    // -------------------------
     @PostMapping("/register")
     public User register(@RequestBody User user) {
         return userService.registerUser(user);
     }
 
-    // -------------------------
-    // LOGIN
-    // -------------------------
     @PostMapping("/login")
     public AuthResponse login(@RequestBody User user) {
 
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        user.getEmail(),
-                        user.getPassword()
-                )
-        );
-
         User dbUser = userService.findByEmail(user.getEmail());
 
-        // Dummy token (JWT logic not required for tests)
         String token = "token";
 
         return new AuthResponse(
